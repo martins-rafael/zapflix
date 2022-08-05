fbf4c43a07d92ff3c21e770dec80b1d9fb2ac6e2
Rafael Martins <rafaeldcmartins@gmail.com>
2022-08-05 18:00:15 -0300

    feat: movie genre CRUD created

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_28_005739) do
-  create_table "artists", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "directors", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_08_05_190934) do
   create_table "movie_genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.date "premiere"
-    t.integer "director_id"
-    t.integer "movie_genre_id"
-    t.text "synopsis"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["director_id"], name: "index_movies_on_director_id"
-    t.index ["movie_genre_id"], name: "index_movies_on_movie_genre_id"
-  end
-
-  add_foreign_key "movies", "directors"
-  add_foreign_key "movies", "movie_genres"
 end
