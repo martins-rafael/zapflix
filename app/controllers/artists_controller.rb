f5222613aac6c48c7fd56f7526d53a3a708c0ded
Rafael Martins <rafaeldcmartins@gmail.com>
2022-08-06 15:56:00 -0300

    feat: artist CRUD created

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,65 @@
+class ArtistsController < ApplicationController
+  before_action :set_artist, only: %i[ show edit update destroy ]
+
+  # GET /artists or /artists.json
+  def index
+    @artists = Artist.all
+  end
+
+  # GET /artists/1 or /artists/1.json
+  def show
+  end
+
+  # GET /artists/new
+  def new
+    @artist = Artist.new
+  end
+
+  # GET /artists/1/edit
+  def edit
+  end
+
+  # POST /artists or /artists.json
+  def create
+    @artist = Artist.new(artist_params)
+
+    respond_to do |format|
+      if @artist.save
+        format.html { redirect_to artists_path, notice: "Artista criado com sucesso!" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /artists/1 or /artists/1.json
+  def update
+    respond_to do |format|
+      if @artist.update(artist_params)
+        format.html { redirect_to artists_path, notice: "Artista atualizado com sucesso!" }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /artists/1 or /artists/1.json
+  def destroy
+    @artist.destroy
+
+    respond_to do |format|
+      format.html { redirect_to artists_url, notice: "Artista excluído com sucesso!" }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def artist_params
+      params.require(:artist).permit(:name)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_05_190934) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_06_174519) do
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "movie_genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
