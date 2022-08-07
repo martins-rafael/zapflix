54c46822fdcb441059f3073e82e228717c6e837d
Rafael Martins <rafaeldcmartins@gmail.com>
2022-08-07 14:29:35 -0300

    fix: fixed movie_params in movies_controller for correct artists saving

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -60,6 +60,6 @@ class MoviesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def movie_params
-      params.require(:movie).permit(:title, :cover_url, :published_at, :director_id, :movie_genre_id, :synopsis)
+      params.require(:movie).permit(:title, :cover_url, :published_at, :director_id, :movie_genre_id, :synopsis, artist_ids:[])
     end
 end
