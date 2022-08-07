82933dc976850c45f2ff9208a667c651346c743a
Rafael Martins <rafaeldcmartins@gmail.com>
2022-08-07 18:05:36 -0300

    feat: added validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,8 @@ class Movie < ApplicationRecord
   belongs_to :movie_genre
 
   has_and_belongs_to_many :artists
+
+  validates :title, :cover_url, :published_at, :director_id, :movie_genre_id, :synopsis, :artist_ids, presence: true
+  validates :title, presence: true, uniqueness: { case_sensitive: false }
+  validates :cover_url, format: { with: /(https?:\/\/.*\.(?:png|jpg))/i, message: "deve começar com https e terminar com .png ou .jpg" }
 end
--- app/models/movie_genre.rb
+++ app/models/movie_genre.rb
@@ -1,3 +1,5 @@
 class MovieGenre < ApplicationRecord
   has_many :movies
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
